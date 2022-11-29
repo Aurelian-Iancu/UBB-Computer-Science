911c4b567ab68dbcf223c3e96b5a22a926421fa5
913-Iancu-Gheorghe-Aurelian <iancuaurelian333@yahoo.com>
2022-11-29 13:02:33 +0200

    RefType, RefValue, Heap MAP lab4

---
+++ Semester3/Advanced Programming Methods/Homeworks/A4/model/value/RefValue.java
@@ -0,0 +1,32 @@
+package model.value;
+
+import model.type.RefType;
+import model.type.Type;
+
+public class RefValue implements Value{
+    private final int address;
+    private final Type locationType;
+
+    public RefValue(int address, Type locationType) {
+        this.address = address;
+        this.locationType = locationType;
+    }
+
+    @Override
+    public Type getType() {
+        return new RefType(locationType);
+    }
+
+    public int getAddress(){
+        return this.address;
+    }
+
+    public Type getLocationType(){
+        return this.locationType;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %s)", address, locationType);
+    }
+}
