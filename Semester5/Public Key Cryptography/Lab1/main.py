33108c15e314b4b61d8d85c07c2174f420bf33f1
913-Iancu-Gheorghe-Aurelian <110055304+Aurelian-Iancu@users.noreply.github.com>
2023-10-24 20:12:24 +0300

    Lab1 crypto

---
+++ Semester5/Public Key Cryptography/Lab1/main.py
@@ -0,0 +1,91 @@
+import datetime
+import time
+
+
+def gcd_brute_force(x, y):
+    """
+    We initialise the greatest_common_divisor variable with the smallest of the numbers and we try to see if we find a
+    smaller number which for both of the numbers.
+    More straight forward, we try every number between min(x,y) and 1 to see if both x and y can divide by it
+    """
+    if x == 0 or x == y:
+        return y
+    elif y == 0:
+        return x
+    greatest_common_divisor = min(x, y)
+    while x % greatest_common_divisor != 0 or y % greatest_common_divisor != 0:
+        greatest_common_divisor -= 1
+    return greatest_common_divisor
+
+
+def gcd_euclidean_subtractions(x, y):
+    """
+    This is the Euclidean algorithm using subtractions
+    The trick is that we subtract the smaller number from the bigger number until they are equal.
+    """
+    while x != y:
+        if x < y:
+            y = y - x
+        else:
+            x = x - y
+    return x
+
+
+def gcd_euclidean_divisions(x, y):
+    """
+    The algorithm repeatedly takes the remainder of y divided by x and continues the process with the new values until
+    x becomes zero, at which point the GCD is found. The algorithm is implemented recursively.
+    """
+    if (x == 0):
+        return y;
+    return gcd_euclidean_divisions(y % x, x);
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    tests = [
+        (18, 12),
+        (30, 17),
+        (45, 70),
+        (255, 300),
+        (255, 177),
+        (101, 301),
+        (4137524, 1227244),
+        # (294733, 10383680172),
+        (4 ** 4, 6 ** 4),
+        # (2 ** 50, 4 ** 20),
+        (6 ** 3, 3 ** 7),
+        # (12345665434562, 57658787675842),
+        (182364, 116033),
+        (70004, 43602),
+        (106626666, 783764444),
+        (12414311, 431244132)
+    ]
+    for test in tests:
+        first = test[0]
+        second = test[1]
+        print("\nx={},y={}".format(first, second))
+
+        print("Start Brute Force GCD")
+        start_time = time.perf_counter()
+        gcd = gcd_brute_force(first, second)
+        end_time = time.perf_counter()
+        print(f"Time elapsed: {end_time - start_time:.6f} seconds")
+        print(f"GCD is {gcd}\n")
+
+        print("Start Euclidean with subtractions GCD")
+        start_time = time.perf_counter()
+        gcd = gcd_euclidean_subtractions(first, second)
+        end_time = time.perf_counter()
+        print(f"Time elapsed: {end_time - start_time:.6f} seconds")
+        print(f"GCD is {gcd}\n")
+
+        print("Start Euclidean with divisions GCD")
+        start_time = time.perf_counter()
+        gcd = gcd_euclidean_divisions(first, second)
+        end_time = time.perf_counter()
+        print(f"Time elapsed: {end_time - start_time:.6f} seconds")
+        print(f"GCD is {gcd}\n")
+
+
